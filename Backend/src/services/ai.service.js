5a5fb932f80328a08776e06e7bcf4b2d46edc8e3
V-M-B <vmbvarun@gmail.com>
2026-03-19 23:26:32 +0530

    feat: add interview report schema and implement report generation function

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -7,9 +7,84 @@ const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_API_KEY
 })
 
+const interviewReportSchema = z.object({
+    matchScore: z.number().description("The match score between the candidate and the job description, ranging from 0 to 100"),
+  technicalQuestions: z.array(
+    z.object({
+      question: z
+        .string()
+        .description("The technical question can be asked in the interview"),
+      intention: z
+        .string()
+        .description("The intention of interviewer behind asking this question"),
+      answer: z
+        .string()
+        .description(
+          "How to answer this question, what points to cover"
+        ),
+    })
+  ).description(
+    "Technical questions that can be asked in the interview along with their intention and answers"
+  ),
 
+  behavioralQuestions: z.array(
+    z.object({
+      question: z
+        .string()
+        .description("The technical question can be asked in the interview"),
+      intention: z
+        .string()
+        .description("The intention of interviewer behind asking this question"),
+      answer: z
+        .string()
+        .description(
+          "How to answer this question, what points to cover"
+        ),
+    })
+  ).description(
+    "Behavioral questions that can be asked in the interview along with their intention and answers"
+  ),
 
-async function generateInterviewReport(jobDescription, resumeText, selfDescription) {}
+  skillGaps: z.array(
+    z.object({
+      skill: z
+        .string()
+        .description("The skill which the candidate is lacking"),
+      severity: z
+        .enum(["low", "medium", "high"])
+        .description("The severity of this skill gap"),
+    })
+  ).description(
+    "List of skill gaps in the candidate's profile along with their severity"
+  ),
+
+  preparationPlan: z.array(
+    z.object({
+      day: z
+        .number()
+        .description(
+          "The day number in the preparation plan, starting from 1"
+        ),
+    })
+  ),
+});
+
+async function generateInterviewReport(jobDescription, resumeText, selfDescription) {
+    const response = await ai.models.generateContent({
+        model: "models/gemini-2.5-flash",
+        contents: `You are an expert career coach. Your task is to generate a comprehensive interview report for a candidate based on the provided job description, resume, and self-description. The report should include the following sections:`,
+        config:{
+            responseFormat: {
+                type: "json_object",
+                jsonSchema: zodToJsonSchema(interviewReportSchema)
+            }
+        }
+    })
+
+    console.log(JSON.parse(response.text));
+    
+        }
+
 
 
 
