82870074005126441fcb678177ad46759dbb5233
V-M-B <vmbvarun@gmail.com>
2026-04-03 21:45:52 +0530

    prepare for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -6,8 +6,7 @@ connectToDB();
 
 // invokeai();
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
